cce4d75a708ae8dde592bf066d7a537b1b7f7ab4
Usuario <Usuario@CPD02-M02>
2024-03-12 11:32:43 -0300

    Adicionar arquivos de projeto.

---
+++ MediaEscolar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaEscolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a média do aluno: ");
+            double media = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a porcentagem de presença do aluno: ");
+            double presenca = double.Parse(Console.ReadLine());
+
+            if (media >= 7 && presenca >= 75 )
+            {
+                Console.WriteLine("Você foi Aprovado!");
+            }
+            else if ( media >= 4 && presenca >= 75 )
+            {
+                Console.WriteLine("Você está em Recuperação!");
+            }
+            else if ( media >= 7 && presenca < 75 )
+            {
+                Console.WriteLine("Você está em Recuperação!");
+            }
+            else if ( media >=4 && presenca < 75 )
+            {
+                Console.WriteLine("Você está Retido!");
+            }
+            else
+            {
+                Console.WriteLine("Você está Retido!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
